91fd89117130cb175343c0307fc8b6d88fa08158
vivek200 <goyalvivek81@gmail.com>
2022-05-04 15:49:15 +0530

    Code Fixes

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,7 +2,7 @@ import Product from "./Product";
 
 export default function Home({posts, addtoCart}) {
   return (
-    <div className="container"  >
+    <div className="container">
         <div className="row">
             {posts.map((post, ind) => 
                 <Product 
